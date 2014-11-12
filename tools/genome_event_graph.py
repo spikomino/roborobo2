ded6d26307861863b0e9acf11ac7aefb5a8651f0
spikomino <spiko@spikomino.net>
2014-11-12 13:00:43 +0100

    exchange event viewer

---
+++ tools/genome_event_graph.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------
+
+import pylab
+import networkx as nx
+from optparse import OptionParser
+from optparse import OptionGroup
+from collections import deque
+from genome2graph import *
+
+def read_options(defaults):
+    parser = OptionParser()
+    parser.add_option("-w", "--win-output", 
+                      dest="win_out",
+                      default=defaults['win_out']  ,
+                      action="store_true",  
+                      help="display in a window very slow [default "
+                      +str(defaults['win_out'])+"]") 
+    parser.add_option("-f", "--infile", 
+                      dest="infile",
+                      type="string",
+                      default=defaults['infile']   ,  
+                      help="the log file  [default "
+                      +str(defaults['infile'])+"]")
+    parser.add_option("-d", "--dotfile", 
+                      dest="dotfile",
+                      type="string",
+                      default=defaults['dotfile']   ,  
+                      help="the filename for the dotfile [default "
+                      +str(defaults['dotfile'])+"]")
+    parser.add_option("-p", "--pngfile", 
+                      dest="pngfile",
+                      type="string",
+                      default=defaults['pngfile']   ,  
+                      help="the file to save the png file [default "
+                      +str(defaults['pngfile'])+"]")
+    
+    return parser.parse_args()
+
+
+#If run directly (toplevel)
+if __name__ == '__main__':
+
+    # Read options & provide with defaults
+    defaults_opts={}
+    defaults_opts['win_out'] = False
+    defaults_opts['infile']  = 'out.log'
+    defaults_opts['dotfile'] = None
+    defaults_opts['pngfile'] = None
+    (options, args) = read_options(defaults_opts)
+    
+    # process comunication graph 
+    g = make_event_graph(options.infile)
+
+    
+    
+    # dot ? png ? window ?
+    if options.dotfile != None :
+        nx.write_dot(g, options.dotfile)
+        if options.pngfile != None :
+            dot2png(options.dotfile, options.pngfile)
+   
+    # If we draw things ... prepare    
+    if options.win_out: 
+        pylab.ion()
+        fig = pylab.figure(num=None, figsize=(5, 5), dpi=100)
+        pylab.show()
+        pylab.clf()
+        #p= dict(zip(g,g))
+        nx.draw(g)
+        pylab.draw()
+        print 'Press enter to exit'
+        raw_input()
