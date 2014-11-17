54ff0c60cb981995ecc79ffd8242e3abca0ad12e
Inaki Fernandez <inaki.fernandez@loria.fr>
2014-11-17 17:27:07 +0100

    Started creating pure neat

---
+++ include/contrib/pure_neat/genomeAdapted.h
@@ -0,0 +1,66 @@
+/*
+Extension by inheritance to default neat/genome.h.
+It includes specific Genome id's for the current genome and its parents
+This can allow to rebuild and trace back the lineage of a given genome
+*/
+#ifndef _GENOMEADAPTED_H_
+#define _GENOMEADAPTED_H_
+
+#include <vector>
+#include "gene.h"
+#include "innovation.h"
+#include "genome.h"
+#include <map>
+namespace NEAT 
+{
+  //A Genome is the primary source of genotype information used to create   
+  //a phenotype.  It contains 3 major constituents:                         
+  //  1) A list of Traits                                                 
+  //  2) A list of NNodes pointing to a Trait from (1)
+  //  3) A list of Genes with Links that point to Traits from (1)         
+  //(1) Reserved parameter space for future use
+  //(2) NNode specifications                                        
+  //(3) Is the primary source of innovation in the evolutionary Genome.     
+  //    Each Gene in (3) has a marker telling when it arose historically.   
+  //    Thus, these Genes can be used to speciate the population, and the   
+  //    list of Genes provide an evolutionary history of innovation and     
+  //    link-building.
+  
+  class GenomeAdapted : public NEAT::Genome{
+    
+  private:
+    typedef NEAT::Genome super;
+    int _idTrace;
+    int _mom;
+    int _dad;
+
+    int nodeId;
+    double innovNumber;
+    
+  public:
+    
+    GenomeAdapted(int num_in,int num_out,int num_hidden,int type);
+    GenomeAdapted(const GenomeAdapted& genome);
+    GenomeAdapted(int id, std::vector<Trait*> t, std::vector<NNode*> n, std::vector<Gene*> g);
+    ~GenomeAdapted();
+    int getIdTrace() const;
+    int getMom() const;
+    int getDad() const;
+    
+    // Duplicate this Genome to create a new one with the specified id 
+    GenomeAdapted* duplicate();
+    GenomeAdapted* mutate(float sigma, int idRobot ,int idNewGenome, int &nodeId, double &innovNumber);
+
+    GenomeAdapted* mutate(float sigma, int idRobot ,int newId);
+
+    void setIdTrace(int id);
+    void setMom(int idMom);
+    void setDad(int idDad);
+
+    void setNodeId(int id){nodeId=id;};
+    void setInnovNumber(double n){innovNumber=n;};
+
+  };
+} // namespace NEAT
+
+#endif
---
+++ src/contrib/pure_neat/genomeAdapted.cpp
@@ -0,0 +1,218 @@
+/*
+  Extension by inheritance to default neat/genome.cpp
+It includes specific GenomeAdapted id's for the current genome and its parents
+This can allow to rebuild and trace back the lineage of a given genome
+
+*/
+#include "neat/genomeAdapted.h"
+
+#include <iostream>
+#include <cmath>
+#include <sstream>
+using namespace NEAT;
+
+
+GenomeAdapted::GenomeAdapted(int num_in,int num_out,int num_hidden,int type)
+  : super(num_in,num_out,num_hidden,type)
+{
+}
+GenomeAdapted::GenomeAdapted(const GenomeAdapted& genome):super(genome)
+{
+  setIdTrace(genome.getIdTrace());
+  setMom(genome.getMom());
+  setDad(genome.getDad());
+}
+GenomeAdapted::GenomeAdapted(int id, std::vector<Trait*> t, std::vector<NNode*> n, std::vector<Gene*> g):super(id, t,  n, g)
+{
+}
+GenomeAdapted::~GenomeAdapted()
+{
+  //Calls Genome's destructor AFTER the end of this method
+}
+int GenomeAdapted::getIdTrace() const
+{
+  return _idTrace;
+}
+int GenomeAdapted::getMom() const
+{
+  return _mom;
+}
+int GenomeAdapted::getDad() const
+{
+  return _dad;
+}
+
+
+
+GenomeAdapted* GenomeAdapted::mutate(float sigma, int idRobot ,int newId){
+
+    return mutate(sigma,idRobot, newId, nodeId, innovNumber);
+
+}
+
+GenomeAdapted *GenomeAdapted::mutate(float sigma, int idRobot ,int idNewGenome, int &nodeId, double &innovNum) 
+{
+  std::map<int, GenomeAdapted*>::iterator curorg;
+
+  GenomeAdapted *new_genome;	//For holding baby's genes
+
+  double mut_power = NEAT::weight_mut_power;
+
+
+
+  //NOTE: default NEAT method (randomly) replaced  by previous selection (best)
+  // which has been stored in _genome
+  new_genome = this -> duplicate ();
+
+  new_genome -> setIdTrace(idNewGenome);
+  new_genome -> setMom(this -> getIdTrace());
+  new_genome -> setDad(-1);  
+  
+  //Decide whether to mate or mutate
+  //If there is only one genome in the list, then always mutate
+  
+  //if(randfloat() < NEAT::mutate_only_prob){
+  //Choose the mutation depending on probabilities  
+  if (randfloat () < NEAT::mutate_add_node_prob){
+      //08/10/14  Bogus variables for innovations deactivated 
+      //perRobot innovNum and nodeId used instead
+      std::vector < Innovation * > innovations;
+      
+      if (new_genome->mutate_add_node
+	  (innovations,
+	   new_genome->nodeId,
+	   new_genome->innovNumber))
+	  std::cout << "Mutation: Node added" <<std::endl; 
+  }   
+  else {
+      if (randfloat () < NEAT::mutate_add_link_prob) {
+	  std::vector < Innovation * >innovations;
+	  if (new_genome->mutate_add_link(innovations,
+					  new_genome->innovNumber,
+					  NEAT::newlink_tries))
+	      
+	      std::cout << "Mutation: Link added" <<std::endl; 
+      }
+      
+      //NOTE:links CANNOT be added directly after a node  because the phenotype
+      // will not be appropriately altered to reflect the change
+      else{
+	  //If we didn't do a structural mutation, we do the other kinds
+	  if (randfloat () < NEAT::mutate_link_weights_prob){
+	      new_genome->mutate_link_weights (mut_power, 1.0, GAUSSIAN);
+	      std::cout << "Mutation: link weight changed" <<std::endl; 
+	  }
+	  if (randfloat () < NEAT::mutate_toggle_enable_prob){
+	      new_genome->mutate_toggle_enable (1);
+	      std::cout << "Mutation: toggle a link" <<std::endl; 
+	  }
+	  if (randfloat () < NEAT::mutate_gene_reenable_prob){
+	      new_genome->mutate_gene_reenable ();
+	      std::cout << "Mutation: reenable a link" <<std::endl; 
+	  }
+      }
+  }
+  
+  return new_genome;
+}
+
+// Duplicate this Genome to create a new one with the specified id 
+GenomeAdapted *GenomeAdapted::duplicate(){
+    
+    //Collections for the new Genome
+    std::vector<Trait*> traits_dup;
+    std::vector<NNode*> nodes_dup;
+    std::vector<Gene*>  genes_dup;
+    
+    //Iterators for the old Genome
+    std::vector<Trait*>::iterator curtrait;
+    std::vector<NNode*>::iterator curnode;
+    std::vector<Gene*>::iterator  curgene;
+    
+    //New item pointers
+    Trait *newtrait;
+    NNode *newnode;
+    Gene  *newgene;
+    Trait *assoc_trait;  //Trait associated with current item
+    
+    NNode *inode; //For forming a gene 
+    NNode *onode; //For forming a gene
+    Trait *traitptr;
+    
+    GenomeAdapted *newgenome;
+    
+    //verify();
+    
+    //Duplicate the traits
+    for(curtrait=traits.begin(); curtrait!=traits.end(); ++curtrait) {
+	newtrait=new Trait(*curtrait);
+	traits_dup.push_back(newtrait);
+    }
+
+    //Duplicate NNodes
+    for(curnode=nodes.begin();curnode!=nodes.end();++curnode) {
+	//First, find the trait that this node points to
+	if (((*curnode)->nodetrait)==0)
+	    assoc_trait=0;
+	else {
+	    curtrait=traits_dup.begin();
+	    while(((*curtrait)->trait_id)!=(((*curnode)->nodetrait)->trait_id))
+		++curtrait;
+	    assoc_trait=(*curtrait);
+	}
+	
+	newnode=new NNode(*curnode,assoc_trait);
+	
+	(*curnode)->dup=newnode;  //Remember this node's old copy
+	//    (*curnode)->activation_count=55;
+	nodes_dup.push_back(newnode);    
+    }
+    
+    //Duplicate Genes
+    for(curgene=genes.begin(); curgene!=genes.end(); ++curgene) {
+	//First find the nodes connected by the gene's link
+	
+	inode=(((*curgene)->lnk)->in_node)->dup;
+	onode=(((*curgene)->lnk)->out_node)->dup;
+	
+	//Get a pointer to the trait expressed by this gene
+	traitptr=((*curgene)->lnk)->linktrait;
+	if (traitptr==0) assoc_trait=0;
+	else {
+	    curtrait=traits_dup.begin();
+	    while(((*curtrait)->trait_id)!=(traitptr->trait_id))
+		++curtrait;
+	    assoc_trait=(*curtrait);
+	}
+	
+	newgene=new Gene(*curgene,assoc_trait,inode,onode);
+	genes_dup.push_back(newgene);
+    }
+
+    //Finally, return the genome
+    newgenome=new GenomeAdapted(genome_id,traits_dup,nodes_dup,genes_dup);
+    
+    newgenome->_idTrace    = _idTrace;
+    newgenome->_mom        = _mom;
+    newgenome->_dad        = _dad;
+    newgenome->nodeId      = nodeId;
+    newgenome->innovNumber = innovNumber;
+    
+    return newgenome;
+}
+
+
+void GenomeAdapted::setIdTrace(int id)
+{
+  _idTrace = id;
+}
+void GenomeAdapted::setMom(int idMom)
+{
+  _mom = idMom;
+}
+void GenomeAdapted::setDad(int idDad)
+{
+  _dad = idDad;
+}
+
+
