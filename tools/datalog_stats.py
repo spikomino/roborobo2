17a774dd881934331960a941de7c4bc6842f5400
spiko mino <spiko@spikomino.net>
2015-04-01 16:11:58 +0200

    corrected script

--- tools/datalog_stats.py
+++ tools/datalog_stats.py
@@ -169,7 +169,7 @@ def process_experiment2(path):
         # default 'fit'. Possible: popsize col for mis
         F.append(process_datalog(f, 'fit')) 
         C.append(process_datalog(f, 'col')) 
-        Fr.append(process_datalog(f, 'bskuse')) 
+        Fr.append(process_datalog(f, 'use')) 
         M.append(process_datalog(f, 'mis')) 
         P.append(process_datalog(f, 'popsize'))
         
