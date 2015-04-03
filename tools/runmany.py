ab6d2c955bc5616659ae84f72d0601d4490e29b6
spiko mino <spiko@spikomino.net>
2015-04-03 16:14:29 +0200

    updated runmany

--- tools/runmany.py
+++ tools/runmany.py
@@ -27,7 +27,7 @@ parameters={
     'gMaxIt'         : 300000,
     'gPaintFloorIteration' : 200000,
     'gBasketCapacity': 10,
-    'gPaintFloor'    : 'false',
+    'gPaintFloor'    : 'true',
     'gBatchMode'     : 'true'}
 
 # copy the tenmplate file and update the values 
